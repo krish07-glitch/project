e94eb6bb99ac473e4986d688ad68478333363208
krish07-glitch <krish105.com@gmail.com>
2026-03-28 20:20:34 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function showMessage() {
-  alert("Thanks for visiting my portfolio!");
-}
-
-function scrollToSection(id) {
-  document.getElementById(id).scrollIntoView({
-    behavior: "smooth"
-  });
-}
